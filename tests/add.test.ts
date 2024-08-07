04e11fa332ffe3c2dd6e37cd2c259ff38e8be81e
sumairq <sumairqaisar992@gmail.com>
2024-08-07 10:37:18 +0500

    Configure eslint with prettier

--- tests/add.test.ts
+++ tests/add.test.ts
@@ -1,5 +1,5 @@
-import  {add} from '../src/utils';
+import { add } from "../src/utils";
 
-it('adds 1 + 2 to equal 3', () => {
-    expect(add(1,2)).toBe(3);
+it("adds 1 + 2 to equal 3", () => {
+  expect(add(1, 2)).toBe(3);
 });
